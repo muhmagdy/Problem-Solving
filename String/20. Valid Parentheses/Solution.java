5840d5080c5a2d86c5f924acc71de781719a6ba8
Muhammad Magdy <muh.magdy1@gmail.com>
2023-04-22 07:26:06 +0200

    Valid Parenthese

---
+++ String/20. Valid Parentheses/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{')
+                stack.add(c);
+            else{
+                if(stack.isEmpty())
+                    return false;
+                char opening = stack.pop();
+                if((c == ')' && opening != '(') || (c == '}' && opening != '{') || (c == ']' && opening != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
