fc377ac65a4ea671d3434521ce40fdcfc783ca6b
Muhammad Magdy <muh.magdy1@gmail.com>
2023-05-04 01:17:29 +0300

    Bitwise XOR of All Pairings

---
+++ Bit Manipulation/2425. Bitwise XOR of All Pairings/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int res = 0;
+        if(nums1.length % 2 == 1){
+            for(int i = 0; i < nums2.length; i++)
+                res ^= nums2[i];
+        }
+        if(nums2.length % 2  == 1){
+            for(int i = 0; i < nums1.length; i++)
+                res ^= nums1[i];
+        }
+        return res;
+    }
+}
