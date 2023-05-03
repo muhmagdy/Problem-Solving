c78e1bd01868a72f21aea50d4d70abc61976fe83
Muhammad Magdy <muh.magdy1@gmail.com>
2023-05-04 01:14:55 +0300

    Counting Bits

---
+++ Bit Manipulation/338. Counting Bits/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] bits = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0)
+                bits[i] = bits[i/2];
+            else
+                bits[i] = bits[i-1] + 1;
+        }
+        return bits;
+    }
+}
