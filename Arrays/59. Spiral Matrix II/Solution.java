faf531082738c7e3e1ad2ec57278a18a44d062a2
Muhammad Magdy <muh.magdy1@gmail.com>
2023-05-10 22:59:02 +0300

    Spiral Matrix II

---
+++ Arrays/59. Spiral Matrix II/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int count = 1;
+        for(int i = 0; i <= n/2; i++){
+            for(int j = i; j < n-i; j++)
+                matrix[i][j] = count++;
+            for(int j = i+1; j < n-i; j++)
+                matrix[j][n-i-1] = count++;
+            for(int j = n-i-2; j >= i; j--)
+                matrix[n-i-1][j] = count++;
+            for(int j = n-i-2; j > i; j--)
+                matrix[j][i] = count++;
+        }
+        return matrix;
+    }
+}
